7b508819294c6ce38784ee3e695c4832c10ad70b
Dr. Jan Itor <noneofyour@business.com>
2013-06-10 10:57:50 +0200

    Moving platform-specific crap to platforms/

--- build.sh
+++ build.sh
@@ -6,31 +6,30 @@ realpath() {
 
 HERE=$(dirname $(realpath $0))
 
+PLATFORMS=$HERE/platforms
 DIST=$HERE/dist
 BUILD=$HERE/build
 
 mkdir $DIST $BUILD
 
+cp -a $PLATFORMS/. $BUILD
+
 
 # Chrome (using crxmake)
 
-CHROME_BUILD=$BUILD/chrome-build
+CHROME_BUILD=$BUILD/chrome
 PEM=$BUILD/key.pem
 
-mkdir $CHROME_BUILD
-cp -a chrome/* $CHROME_BUILD/
-cp -a src/*.js $CHROME_BUILD/
+cp -a src/* $CHROME_BUILD/
 openssl genrsa | openssl pkcs8 -topk8 -nocrypt -v2 aes-128-ecb > $PEM
-(cd $BUILD && $HERE/util/crxmake.sh $CHROME_BUILD $PEM $DIST/bb-salary-calc.crx)
+(cd $BUILD && $PLATFORMS/util/crxmake.sh $CHROME_BUILD $PEM $DIST/bb-salary-calc.crx)
 
 
 # Firefox (using cfx)
 
-FF_BUILD=$BUILD/ff-build
+FF_BUILD=$BUILD/firefox
 
-mkdir $FF_BUILD
-cp -R firefox/* $FF_BUILD
-cp -R src/* $FF_BUILD/data
+cp -a src/* $FF_BUILD/data
 (cd $FF_BUILD && cfx xpi && mv *.xpi $DIST)
 
 
